d7d69e849475a00468a6eef67555f449342d8b39
JAndres PinillaB <andres_pb07@outlook.com>
2023-04-05 17:34:46 -0500

    conf menu opciones mediante bucle do-while y promesas

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ require('colors');
 const {showAppMenu, pauseAppMenu} = require('./helpers/mensajes');
 
 const main = async() => {
+
     console.log('Hello World');
 
-    showAppMenu();
+    let optionMenu = '';
+    
+    do {
 
-    pauseAppMenu();
+        optionMenu = await showAppMenu();
+        console.log({optionMenu});
+
+        // this conditional is when we put 0 on console, we leave our app
+        if(optionMenu !== '0')
+        await pauseAppMenu();
+        
+    } while (optionMenu !== '0');
+
+
+    //pauseAppMenu();
 
 } 
 
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,42 +1,56 @@
+
 require('colors');
 
 const showAppMenu = () => {
-    console.clear();
-    console.log('========================'.green);
-    console.log('   Choose one option   '.green);
-    console.log('========================\n'.green);
 
-    console.log(`${'1.'.green} Create tasks`);
-    console.log(`${'2.'.green} List tasks`);
-    console.log(`${'3.'.green} Complete list tasks`);
-    console.log(`${'4.'.green} Pending list tasks`);
-    console.log(`${'5.'.green} Complete task(s)`);
-    console.log(`${'6.'.green} Delete task`);
-    console.log(`${'0.'.green} Exit\n`);
+    return new Promise(resolve => {
 
-    // const that we say and get information by the user type in our console
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
+        console.clear();
+        console.log('========================'.green);
+        console.log('   Choose one option   '.green);
+        console.log('========================\n'.green);
+
+        console.log(`${'1.'.green} Create tasks`);
+        console.log(`${'2.'.green} List tasks`);
+        console.log(`${'3.'.green} Complete list tasks`);
+        console.log(`${'4.'.green} Pending list tasks`);
+        console.log(`${'5.'.green} Complete task(s)`);
+        console.log(`${'6.'.green} Delete task`);
+        console.log(`${'0.'.green} Exit\n`);
+
+        // const that we say and get information by the user type in our console
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        // we make a question to user
+        readline.question('Select one option: ', (answerOption) => {
+            readline.close();
+
+            resolve(answerOption);
+        })
+
     });
-
-    // we make a question to user
-    readline.question('Select one option: ', (answerOption) =>{
-        readline.close();
-    })
 }
 
-const pauseAppMenu = () =>{ 
-       // const that we say and get information by the user type in our console
-       const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+const pauseAppMenu = () => {
 
-    // we make a question to user
-    readline.question(`\nPush ${'ENTER'.blue} to continue\n`, (answerOption) =>{
-        readline.close();
-    })
+    return new Promise(resolve => {
+
+        // const that we say and get information by the user type in our console
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        // we make a question to user
+        readline.question(`\nPush ${'ENTER'.blue} to continue\n`, (answerOption) => {
+            readline.close();
+            resolve();
+        })
+
+    });
 }
 
 module.exports = {
