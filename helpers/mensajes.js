e188ece2117cd3aa57847b5b63c3b3304f1b4030
JAndres PinillaB <andres_pb07@outlook.com>
2023-04-05 16:10:05 -0500

    conf opciones del menu

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -4,7 +4,15 @@ const showAppMenu = () => {
     console.clear();
     console.log('========================'.green);
     console.log('   Choose one option   '.green);
-    console.log('========================'.green);
+    console.log('========================\n'.green);
+
+    console.log(`1. Create tasks`);
+    console.log(`2. List tasks`);
+    console.log(`3. Complete list tasks`);
+    console.log(`4. Pending list tasks`);
+    console.log(`5. Complete task(s)`);
+    console.log(`6. Delete task`);
+    console.log(`0. Exit\n`);
 }
 
 module.exports = {
