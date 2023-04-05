39976a13df1dfd56e59333ff4df1c5a187d06ab0
JAndres PinillaB <andres_pb07@outlook.com>
2023-04-05 16:18:32 -0500

    se agregan colores en las opciones del menu

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -6,13 +6,13 @@ const showAppMenu = () => {
     console.log('   Choose one option   '.green);
     console.log('========================\n'.green);
 
-    console.log(`1. Create tasks`);
-    console.log(`2. List tasks`);
-    console.log(`3. Complete list tasks`);
-    console.log(`4. Pending list tasks`);
-    console.log(`5. Complete task(s)`);
-    console.log(`6. Delete task`);
-    console.log(`0. Exit\n`);
+    console.log(`${'1.'.green} Create tasks`);
+    console.log(`${'2.'.green} List tasks`);
+    console.log(`${'3.'.green} Complete list tasks`);
+    console.log(`${'4.'.green} Pending list tasks`);
+    console.log(`${'5.'.green} Complete task(s)`);
+    console.log(`${'6.'.green} Delete task`);
+    console.log(`${'0.'.green} Exit\n`);
 }
 
 module.exports = {
